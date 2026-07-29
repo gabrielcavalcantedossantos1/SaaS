ec5fc45eab3caab16b7ce03834b1f44003c04cea
Gabriel Cavalcante <gabrielcavalcantedossantos1@gmail.com>
2026-07-28 23:36:48 -0300

    componente Input para facilitar o codigo e repetiçao

---
+++ src/components/input.tsx
@@ -0,0 +1,20 @@
+import { useId } from "react"
+
+type InputType = {
+    text: string,
+    type?: string,
+    placeholder: string,
+    name: string
+}
+
+export function Input({ name, placeholder, text, type }: InputType) {
+
+    const id = useId()
+
+    return (
+        <div>
+            <label htmlFor={id}>{text}</label>
+            <input type={type ?? 'text'} id={id} placeholder={placeholder} name={name} />
+        </div>
+    )
+}
