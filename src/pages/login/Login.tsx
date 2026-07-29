91323b02964aa7e19267cc605f5bc7a2d8febae9
Gabriel Cavalcante <gabrielcavalcantedossantos1@gmail.com>
2026-07-29 12:56:00 -0300

    logica da pagina login finalizado

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,16 +1,27 @@
-import type { FormEvent } from "react";
+import { useState, type FormEvent } from "react";
 import { Input } from "../../components/input/input";
 import { useAppContext } from "../../context/AppContext";
 
 import './style.css'
+import { useAuth } from "../../context/AuthContent";
+import { useNavigate } from "react-router-dom";
 
 export function Login() {
 
+    const navigate = useNavigate()
+
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
     const { appName } = useAppContext()
+    const { login } = useAuth()
 
     function handleSubmit(e: FormEvent) {
         e.preventDefault()
-        console.log("O formulario foi enviado")
+
+        login(email, password)
+
+        navigate('/')
     }
     return (
         <div className="login">
@@ -26,6 +37,8 @@ export function Login() {
                     name="email"
                     type="email"
                     placeholder="Digite seu e-mail"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
                 />
 
                 <Input
@@ -33,6 +46,8 @@ export function Login() {
                     name="password"
                     type="password"
                     placeholder="Digite sua senha"
+                    value={password}
+                    onChange={e => setPassword(e.target.value)}
                 />
 
                 <button type="submit">Entrar</button>
