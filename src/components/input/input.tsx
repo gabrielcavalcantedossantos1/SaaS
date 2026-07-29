4ae84f65620cb0ef9139a0335a5854d5f1fcb09b
Gabriel Cavalcante <gabrielcavalcantedossantos1@gmail.com>
2026-07-29 12:50:54 -0300

    controlando o projeto via usestate

--- src/components/input/input.tsx
+++ src/components/input/input.tsx
@@ -1,21 +1,23 @@
-import { useId } from "react"
+import { useId, type ChangeEvent } from "react"
 import './style.css'
 
 type InputType = {
     text: string,
     type?: string,
     placeholder: string,
-    name: string
+    name: string,
+    onChange: (e: ChangeEvent<HTMLInputElement>) => void,
+    value: string
 }
 
-export function Input({ name, placeholder, text, type }: InputType) {
+export function Input({ name, placeholder, text, type, onChange, value }: InputType) {
 
     const id = useId()
 
     return (
         <div>
             <label htmlFor={id}>{text}</label>
-            <input type={type ?? 'text'} id={id} placeholder={placeholder} name={name} />
+            <input type={type ?? 'text'} id={id} placeholder={placeholder} name={name} value={value} onChange={onChange} />
         </div>
     )
 }
