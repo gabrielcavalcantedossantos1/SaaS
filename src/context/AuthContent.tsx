015b09f878fa960086e8f0d185891bc8f48eb43e
Gabriel Cavalcante <gabrielcavalcantedossantos1@gmail.com>
2026-07-28 23:22:24 -0300

    criando o contexto de autenticaçao

---
+++ src/context/AuthContent.tsx
@@ -0,0 +1,35 @@
+import { createContext, useContext, useState, type ReactNode } from 'react'
+
+// contexto para o projeto de organização de estudos, fornecendo informações globais sobre o aplicativo, como o nome do aplicativo.
+
+type User = {
+    id: string,
+    name: string,
+    email: string
+}
+
+type AuthContextType = {
+    user: User | null
+}
+
+const AuthContent = createContext<AuthContextType | undefined>(undefined)
+
+export function AppProvider({ children }: { children: ReactNode }) {
+
+    const [user, setUser] = useState<User | null>(null)
+
+    const value: AuthContextType = {
+        user
+    }
+    return <AuthContent.Provider value={value}>{children}</AuthContent.Provider>
+}
+
+export function useAuth() {
+    const context = useContext(AuthContent)
+
+    if (!context) {
+        throw new Error('useAuth must be used within an AppProvider')
+    }
+
+    return context
+}
