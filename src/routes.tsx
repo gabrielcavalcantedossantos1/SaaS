53bf38aee2c398236e57c30923f63214bf3b68eb
Gabriel Cavalcante <gabrielcavalcantedossantos1@gmail.com>
2026-07-28 23:47:58 -0300

    add login as rotas

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,9 +2,13 @@ import { createBrowserRouter } from 'react-router-dom'
 import { HomePage } from './pages/HomePage'
 import { AboutPage } from './pages/AboutPage'
 import { Layout } from './components/layout/Layout'
+import { Login } from './pages/login/Login'
 
 const routes = createBrowserRouter([
   {
+    path: "/login",
+    element: <Login />
+  }, {
     path: '/',
     element: <Layout />,
     children: [
@@ -12,7 +16,7 @@ const routes = createBrowserRouter([
         index: true,
         element: <HomePage />,
       }, {
-        path: '/about',
+        path: 'about',
         element: <AboutPage />,
       }
     ]
