82b4410e426ba63300390428c160c3b5f71d05df
Gabriel Cavalcante <gabrielcavalcantedossantos1@gmail.com>
2026-07-28 23:57:13 -0300

    erro de digitaçao no nome da funçao

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,7 +1,7 @@
 import { Navigate, Outlet } from "react-router-dom";
 import { useAuth } from "../context/AuthContent";
 
-export function ProtectedRout() {
+export function ProtectedRoute() {
     const { user } = useAuth()
 
     if (!user) {
