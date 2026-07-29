9ddeacddf95efcb3f0018694681b778d2bf8667e
Gabriel Cavalcante <gabrielcavalcantedossantos1@gmail.com>
2026-07-28 23:58:16 -0300

    configuraçao da rota protegida

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,23 +3,27 @@ import { HomePage } from './pages/HomePage'
 import { AboutPage } from './pages/AboutPage'
 import { Layout } from './components/layout/Layout'
 import { Login } from './pages/login/Login'
+import { ProtectedRoute } from './components/ProtectedRoute'
 
 const routes = createBrowserRouter([
   {
     path: "/login",
     element: <Login />
   }, {
-    path: '/',
-    element: <Layout />,
-    children: [
-      {
-        index: true,
-        element: <HomePage />,
-      }, {
-        path: 'about',
-        element: <AboutPage />,
-      }
-    ]
+    element: <ProtectedRoute />,
+    children: [{
+      path: '/',
+      element: <Layout />,
+      children: [
+        {
+          index: true,
+          element: <HomePage />,
+        }, {
+          path: 'about',
+          element: <AboutPage />,
+        }
+      ]
+    }]
   }
 ])
 
