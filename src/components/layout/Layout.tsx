fba31cbdb39a654932c031a84b1512bfe6da13e6
Gabriel Cavalcante <gabrielcavalcantedossantos1@gmail.com>
2026-07-28 22:19:39 -0300

    colocando os icones

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -3,22 +3,35 @@ import { useAppContext } from "../../context/AppContext";
 
 // style
 import './style.css'
+import { House, Info, Menu } from "lucide-react";
+import { useState } from "react";
 
 
 export function Layout() {
+
+    const [isOpen, setIsOpen] = useState(true)
+
     const { appName } = useAppContext();
     return (
         <div className="layout">
 
             {/* Está é a barra lateral */}
-            <aside>
+            <aside className={isOpen ? "sidebarOpen" : "sidebarClosed"}>
+
+                <button onClick={() => setIsOpen(!isOpen)}><Menu /></button>
                 <nav>
                     <ul>
                         <li>
-                            <NavLink to="/" className={({ isActive }) => (isActive ? 'active' : 'btn')}>Home</NavLink>
+                            <NavLink to="/" className={({ isActive }) => (isActive ? 'active' : 'btn')}>
+                                <House />
+                                {isOpen && <span>Home</span>}
+                            </NavLink>
                         </li>
                         <li>
-                            <NavLink to="/about" className={({ isActive }) => (isActive ? 'active' : 'btn')}>About</NavLink>
+                            <NavLink to="/about" className={({ isActive }) => (isActive ? 'active' : 'btn')}>
+                                <Info />
+                                {isOpen && <span>Sobre</span>}
+                            </NavLink>
                         </li>
                     </ul>
                 </nav>
