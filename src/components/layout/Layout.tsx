3ab23af3ed5b41d9465b2fce08e2ac73f66e9bcb
Gabriel Cavalcante <gabrielcavalcantedossantos1@gmail.com>
2026-07-31 20:27:38 -0300

    fix: ajusta layout com sidebar fixa e conteúdo alinhado

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -2,64 +2,72 @@ import { NavLink, Outlet, Link } from "react-router-dom";
 import { useAppContext } from "../../context/AppContext";
 
 // style
-import './style.css'
+import "./style.css";
 import { House, Info, Menu, User } from "lucide-react";
 import { useState } from "react";
 
-
 export function Layout() {
+  const [isOpen, setIsOpen] = useState(true);
 
-    const [isOpen, setIsOpen] = useState(true)
+  const { appName, userName } = useAppContext();
+  return (
+    <div className="layout">
+      {/* Está é a barra lateral */}
+      <aside className={isOpen ? "sidebarOpen" : "sidebarClosed"}>
+        <nav>
+          <ul>
+            <li>
+              <NavLink
+                to="/"
+                className={({ isActive }) => (isActive ? "active" : "btn")}
+              >
+                <House />
+                {isOpen && <span>Home</span>}
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/about"
+                className={({ isActive }) => (isActive ? "active" : "btn")}
+              >
+                <Info />
+                {isOpen && <span>Sobre</span>}
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+      </aside>
+      <div
+        className={
+          isOpen ? "content contentSidebarOpen" : "content contentSidebarClosed"
+        }
+      >
+        {/* Este é o cabeçalho */}
+        <header>
+          <button onClick={() => setIsOpen(!isOpen)}>
+            <Menu />
+          </button>
 
-    const { appName, userName } = useAppContext();
-    return (
-        <div className="layout">
+          <div className="headerContent">
+            <Link to="/" className="logo">
+              {appName}
+            </Link>
 
-            {/* Está é a barra lateral */}
-            <aside className={isOpen ? "sidebarOpen" : "sidebarClosed"}>
-                <nav>
-                    <ul>
-                        <li>
-                            <NavLink to="/" className={({ isActive }) => (isActive ? 'active' : 'btn')}>
-                                <House />
-                                {isOpen && <span>Home</span>}
-                            </NavLink>
-                        </li>
-                        <li>
-                            <NavLink to="/about" className={({ isActive }) => (isActive ? 'active' : 'btn')}>
-                                <Info />
-                                {isOpen && <span>Sobre</span>}
-                            </NavLink>
-                        </li>
-                    </ul>
-                </nav>
-            </aside>
+            <span>
+              <User />
+              {userName}
+            </span>
+          </div>
+        </header>
 
-            <div className="content">
+        <main>
+          <Outlet />
+        </main>
 
-                {/* Este é o cabeçalho */}
-                <header>
-                    <button onClick={() => setIsOpen(!isOpen)}>
-                        <Menu />
-                    </button>
-
-                    <div className="headerContent">
-                        <Link to="/" className="logo">
-                            {appName}
-                        </Link>
-
-                        <span><User />{userName}</span>
-                    </div>
-                </header>
-
-                <main>
-                    <Outlet />
-                </main>
-
-                <footer>
-                    <p>&copy; 2026 {appName}. Todos os direitos reservados.</p>
-                </footer>
-            </div>
-        </div>
-    );
+        <footer>
+          <p>&copy; 2026 {appName}. Todos os direitos reservados.</p>
+        </footer>
+      </div>
+    </div>
+  );
 }
