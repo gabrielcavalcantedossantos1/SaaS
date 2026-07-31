51e4f31a8dfbaac5666629623c3095f46abb2510
Gabriel Cavalcante <gabrielcavalcantedossantos1@gmail.com>
2026-07-31 19:29:12 -0300

    adicionando o caminho para a pagina Home

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,34 +1,39 @@
-import { createBrowserRouter } from 'react-router-dom'
-import { HomePage } from './pages/HomePage'
-import { AboutPage } from './pages/AboutPage'
-import { Layout } from './components/layout/Layout'
-import { Login } from './pages/login/Login'
-import { ProtectedRoute } from './components/ProtectedRoute'
-import { Register } from './pages/register/Register'
+import { createBrowserRouter } from "react-router-dom";
+import { AboutPage } from "./pages/AboutPage";
+import { Layout } from "./components/layout/Layout";
+import { Login } from "./pages/login/Login";
+import { ProtectedRoute } from "./components/ProtectedRoute";
+import { Register } from "./pages/register/Register";
+import { Home } from "./pages/home/Home";
 
 const routes = createBrowserRouter([
   {
     path: "/login",
-    element: <Login />
-  }, {
-    path: '/register',
-    element: <Register />
-  }, {
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
     element: <ProtectedRoute />,
-    children: [{
-      path: '/',
-      element: <Layout />,
-      children: [
-        {
-          index: true,
-          element: <HomePage />,
-        }, {
-          path: 'about',
-          element: <AboutPage />,
-        }
-      ]
-    }]
-  }
-])
+    children: [
+      {
+        path: "/",
+        element: <Layout />,
+        children: [
+          {
+            index: true,
+            element: <Home />,
+          },
+          {
+            path: "about",
+            element: <AboutPage />,
+          },
+        ],
+      },
+    ],
+  },
+]);
 
-export default routes
+export default routes;
