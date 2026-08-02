51f33df98b1cd52ecfa354d626a0e01ef1252d2a
Gabriel Cavalcante <gabrielcavalcantedossantos1@gmail.com>
2026-08-02 18:35:34 -0300

    estrutura da pagina tasks

---
+++ src/pages/tasks/Tasks.tsx
@@ -0,0 +1,46 @@
+import { TaskCard } from '../../components/tasks/taskCard/TaskCard'
+import './styles.css'
+
+export function Tasks() {
+
+    const fictitiousTasks = [
+        {
+            id: 1,
+            title: 'Estudar React',
+            description: 'Estudar React para aprender a criar aplicações web',
+            completed: false
+        }, {
+            id: 2,
+            title: 'Estudar TypeScript',
+            description: 'Estudar TypeScript para aprender a criar aplicações web',
+            completed: false
+        }, {
+            id: 3,
+            title: 'Estudar JavaScript',
+            description: 'Estudar JavaScript para aprender a criar aplicações web',
+            completed: false
+        }
+    ]
+    return (
+        <div className="tasksContainer">
+            <header className="tasksHeader">
+                <div>
+                    <h1>Tarefas</h1>
+                    <p>Organize seus estudos e acompanhe seu progresso</p>
+                </div>
+
+                <button type='button'>+ Nova Tarefa</button>
+            </header>
+
+            <div className="search">
+                <input type="text" placeholder='Pesquisar tarefa...' />
+            </div>
+
+            <main className="tasksList">
+                {fictitiousTasks.map((task) => (
+                    <TaskCard key={task.id} task={task} />
+                ))}
+            </main>
+        </div>
+    )
+}
