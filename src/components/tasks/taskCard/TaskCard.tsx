31b9ee3643a5c4a84d280dbbc5d84d7e9c9449e5
Gabriel Cavalcante <gabrielcavalcantedossantos1@gmail.com>
2026-08-04 21:17:48 -0300

    adicionando a funcionalidade de exclusao

--- src/components/tasks/taskCard/TaskCard.tsx
+++ src/components/tasks/taskCard/TaskCard.tsx
@@ -1,4 +1,5 @@
-import { updateStudyStatus } from '../../../services/studies'
+import { deleteStudy, updateStudyStatus } from '../../../services/studies'
+import { notify } from '../../../services/toast'
 import type { StudyItem } from '../../../types/study'
 import './styles.css'
 
@@ -8,10 +9,36 @@ type TaskCardProps = {
 
 export function TaskCard({ task }: TaskCardProps) {
 
-    async function handleToggleStatus() {
-        const newTask = task.status === 'done' ? 'todo' : 'done'
 
-        await updateStudyStatus(task.id, newTask)
+    // Função para alternar o status da tarefa entre "todo" e "done"
+    async function handleToggleStatus() {
+        const newStatus = task.status === 'done' ? 'todo' : 'done'
+
+        try {
+            await updateStudyStatus(task.id, newStatus)
+            const successMessage =
+                newStatus === 'done'
+                    ? 'Tarefa concluída com sucesso!'
+                    : 'Tarefa marcada como pendente.';
+            notify.success(successMessage)
+        } catch {
+            const errorMessage = 'Erro ao atualizar o status da tarefa. Tente novamente.'
+            notify.error(errorMessage)
+        }
+    }
+
+    // função para excluir a tarefa
+    async function handleDeleteTask() {
+
+        const confirmDelete = window.confirm('Tem certeza que deseja excluir esta tarefa?')
+        if (!confirmDelete) return
+        try {
+            await deleteStudy(task.id)
+            notify.success('Tarefa excluída com sucesso.')
+        } catch {
+            const errorMessage = 'Erro ao excluir a tarefa. Tente novamente.'
+            notify.error(errorMessage)
+        }
     }
 
     return (
@@ -28,7 +55,9 @@ export function TaskCard({ task }: TaskCardProps) {
 
             <div className="taskActions">
                 <button type='button'>Editar</button>
-                <button type='button'>Excluir</button>
+                <button type='button' onClick={handleDeleteTask}>
+                    Excluir
+                </button>
             </div>
         </article>
 
